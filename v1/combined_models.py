9d205aead6d9e2aef2f10cde9df3a3d43d6e3563
Andrew Wei <andrewwei98@gmail.com>
2019-12-14 18:03:04 -0800

    add code to get times and accuracies for simple, complex, and combined models

--- v1/combined_models.py
+++ v1/combined_models.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from scipy import stats
 import pickle
+import time
 
 import helper_funcs as helpers
 from models import *
@@ -76,20 +77,116 @@ def get_combined_model_accuracy(confidence_bound=0.56):
     
     return data
     
+def get_combined_model_latency():
+    '''
+    Get time the combined model takes to predict the testing set for specific dataset splits.
+    Returns arrays where each index contains the times and accuracies for a given data split.
+    '''
+    x_train, y_train, x_test, y_test = helpers.get_data()
+    trained_complex_all_digit_model = tf.keras.models.load_model('./models/trained_complex_all_digit_model')
+    trained_simple_all_digit_model = tf.keras.models.load_model('./models/trained_simple_all_digit_model')
+
+    combined_times = []
+    combined_accuracies = []
+
+    data_splits = np.arange(0.1, 1, 0.1) # [0.1, 0.9]
+
+    # For each test split, run simple, complex, and combined predictions.
+    for data_split in data_splits:
+
+        total_combined_time = 0
+        total_combined_accuracy = 0
+
+        num_epochs = 10
+        for i in range(num_epochs):
+            # Shuffle data
+            indices = tf.random.shuffle(tf.range(y_test.shape[0]))
+            x_test = tf.gather(x_test, indices)
+            y_test = tf.gather(y_test, indices)
+
+            # Split data
+            split_index = tf.cast(data_split * y_test.shape[0], tf.int32)
+
+            simple_x = x_test[:split_index]
+            simple_y = y_test[:split_index]
+
+            complex_x = x_test[split_index:]
+            complex_y = y_test[split_index:]
+
+            print('=====================')
+            print('Split Index:', split_index)
+            print('Simple:', simple_x.shape[0])
+            print('Complex:', complex_x.shape[0])
+
+            # Combined
+            before_time = time.time()
+            # Complex portion
+            complex_probs = trained_complex_all_digit_model.predict(complex_x)
+            complex_preds = complex_probs.argmax(axis=1)
+            complex_output = tf.equal(complex_preds, complex_y)
+            # Simple portion
+            simple_probs = trained_simple_all_digit_model.predict(simple_x)
+            simple_preds = simple_probs.argmax(axis=1)
+            simple_output = tf.equal(simple_preds, simple_y)
+            # Combined
+            # print('output shapes', complex_output.shape, simple_output.shape)
+            combined_output = tf.concat([complex_output, simple_output], axis=0)
+            combined_accuracy = tf.reduce_mean(tf.cast(combined_output, tf.float32))
+
+            after_time = time.time()
+            total_combined_time += after_time - before_time
+            total_combined_accuracy += combined_accuracy
+
+        combined_times.append(total_combined_time / num_epochs)
+        combined_accuracies.append(total_combined_accuracy.numpy() / num_epochs)
+
+    print('Combined Times:', combined_times)
+    print('Combined Accuracies:', combined_accuracies)
+
+def get_times_and_accuracies(model):
+    '''
+    Print the average time and accuracies for the given model on the
+    test portion of the MNIST dataset.
+    '''
+    x_train, y_train, x_test, y_test = helpers.get_data()
+
+    total_time = 0
+    total_accuracy = 0
+
+    num_epochs = 10
+    for i in range(num_epochs):
+        # Shuffle data
+        indices = tf.random.shuffle(tf.range(y_test.shape[0]))
+        x_test = tf.gather(x_test, indices)
+        y_test = tf.gather(y_test, indices)
+
+        before_time = time.time()
+        accuracy = model.evaluate(x_test, y_test, verbose=0)[1]
+        after_time = time.time()
+        total_time += after_time - before_time
+        total_accuracy += accuracy
+
+    average_time = total_time / num_epochs
+    average_accuracy = total_accuracy / num_epochs
+
+    print('Average Time:', average_time)
+    print('Average Accuracy:', average_accuracy)
+
 
 def main():
-    accuracies = []
-    # confidence_vals = [0.4, 0.56, 0.6, 0.8]
-    confidence_vals = np.arange(0, 1, 0.1)
-    for confidence_val in confidence_vals:
-        # print('----------------------------')
-        # print('Confidence Val:', confidence_val)
-        # get_combined_model_accuracy
-        # print()
-
-        accuracies.append(get_combined_model_accuracy(confidence_val))
-
-    print(accuracies)
+    # --------------------------
+    # Combined
+    # get_combined_model_latency()
+    # --------------------------
+    # Simple 
+    trained_simple_all_digit_model = tf.keras.models.load_model('./models/trained_simple_all_digit_model')
+    print('Simple')
+    get_times_and_accuracies(trained_simple_all_digit_model)
+    # ---------------------------
+    # Complex
+    trained_complex_all_digit_model = tf.keras.models.load_model('./models/trained_complex_all_digit_model')
+    print('Complex')
+    get_times_and_accuracies(trained_complex_all_digit_model)
 
 if __name__ == '__main__':
     main()
