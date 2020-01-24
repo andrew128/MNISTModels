5d650412485b9c618456e61d4ba923b3b7f52a2e
shekarramaswamy4 <shekar.ram.428@gmail.com>
2020-01-24 14:33:16 -0500

    created sub layers for simple net

---
+++ pytorch-exp/nets/layers/Output.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from nets.layers.Flatten import Flatten
+
+class Output(nn.Module):
+    def __init__(self, num_inputs, num_fc_layers = 2, dropout = 0.2):
+        super(Output, self).__init__()
+
+        self.num_inputs = num_inputs 
+        self.num_fc_layers = num_fc_layers 
+
+        self.flatten = Flatten()
+
+        # TODO: loop number of fc layers
+        # TODO: parameterize fc neurons
+        self.fc1 = nn.Linear(num_inputs, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+        self.dropout1 = nn.Dropout2d(dropout)
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
+        return x
---
+++ pytorch-exp/nets/layers/Simple.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from nets.layers.Output import Output
+
+# 1 conv layer
+class Simple(nn.Module):
+    def __init__(self):
+        super(BaseNet, self).__init__()
+
+        input_channels = 1
+        self.conv1 = nn.Conv2d(input_channels, 32, 3)
+
+    # this model architecture follows v0 all_digit_model.py
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.max_pool2d(x, 2)
+        return x
