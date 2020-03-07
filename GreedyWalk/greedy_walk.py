145a1a74eb58e819eb8d1c0c34868fd5f17e2602
Andrew Wei <andrewwei98@gmail.com>
2020-03-07 13:06:08 -0500

    add function to find optimal confidence value using grid search

--- GreedyWalk/greedy_walk.py
+++ GreedyWalk/greedy_walk.py
@@ -1,18 +1,36 @@
 import numpy as np
 import time
 
-import GreedyWalk.set_of_models as SetOfModels
+import GreedyWalk.set_of_nodes as SetOfNodes
 
 def get_optimal_confidence_value_grid(node, dataset, model_time_constraint, step_size = 0.1):
-    pass
+    '''
+    For each possible confidence value between 0 and 1 with input step_size, run the 
+    node with the current confidence value. Store the confidence_value that gives the best accuracy
+    within the input time constraint.
+    '''
+    confidence_values = np.arange(0, 1.1, 0.1)
+    best_confidence_value = 0
+    best_accuracy = 0
+    for conf_value in confidence_values:
+        before = time.time()
+        current_accuracy = node.accuracy(dataset, conf_value)
+        after = time.time()
+
+        if after - before < model_time_constraint:
+            if current_accuracy > best_accuracy:
+                best_accuracy = current_accuracy
+                best_confidence_value = conf_value
+    
+    return best_confidence_value
 
 def get_optimal_confidence_value_random():
     pass
 
-def greedy_walk(set_of_models, search_time_constraint, model_time_constraint, \
+def greedy_walk(set_of_nodes, search_time_constraint, model_time_constraint, \
                 validation_dataset, conf_value_dataset, epsilon = 0.1):
     '''
-    param: set_of_models
+    param: set_of_nodes - SetOfNodes object representing all possible model pairings
     param: search_time_constraint - amount of time given to search
     param: model_time_constraint - amount of time output model pairing must 
                                     complete validation_dataset within
@@ -27,7 +45,7 @@ def greedy_walk(set_of_models, search_time_constraint, model_time_constraint, \
     '''
     timeout_start = time.time()
 
-    current_node = set_of_models.get_most_simple_pairing()
+    current_node = set_of_nodes.get_most_simple_pairing()
     visited_nodes = set([current_node])
 
     best_node_so_far = current_node
@@ -36,23 +54,23 @@ def greedy_walk(set_of_models, search_time_constraint, model_time_constraint, \
     while time.time() < timeout_start + search_time_constraint:
         prob = np.random.random()
         if prob < epsilon:
-            current_node = set_of_models.get_random_neighbor_pairing(visited_nodes)
+            current_node = set_of_nodes.get_random_neighbor_pairing(visited_nodes)
         else:
             current_optimal_conf_value = get_optimal_confidence_value_random(current_node, \
                                                     conf_value_dataset, model_time_constraint)
             current_node.optimal_confidence_value = current_optimal_conf_value
 
             # Test model pairing with current_optimal_conf_value
-            test_accuracy, test_time = set_of_models.test_node(current_node, current_optimal_conf_value)
+            test_accuracy, test_time = set_of_nodes.test_node(current_node, current_optimal_conf_value)
             current_node.accuracy = test_accuracy
             current_node.time = test_time
 
             if test_time < model_time_constraint:
                 if test_accuracy > best_accuracy_so_far:
                     best_node_so_far = current_node
-                current_node = set_of_models.get_neighbor_greater_complexity(visited_nodes)
+                current_node = set_of_nodes.get_neighbor_greater_complexity(visited_nodes)
             else:
-                current_node = set_of_models.get_neighbor_smaller_complexity(visited_nodes)
+                current_node = set_of_nodes.get_neighbor_smaller_complexity(visited_nodes)
 
         visited_nodes.add(current_node)
 
