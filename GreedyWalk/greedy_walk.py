526f09fe649458e423b625287830fd3c02f06ec4
Andrew Wei <andrewwei98@gmail.com>
2020-03-30 23:16:38 -0700

    split dataset into inputs and labels for function params

--- GreedyWalk/greedy_walk.py
+++ GreedyWalk/greedy_walk.py
@@ -4,14 +4,18 @@ import time
 import GreedyWalk.graph as Graph
 
 def greedy_walk(graph, search_time_constraint, model_time_constraint, \
-                validation_dataset, conf_value_dataset, epsilon = 0.1):
+                validation_inputs, validation_labels, conf_value_inputs,\
+                conf_value_labels, epsilon = 0.1):
     '''
-    param: graph - Graph object representing all possible model pairings
-    param: search_time_constraint - amount of time given to search
-    param: model_time_constraint - amount of time output model pairing must 
+    :param: graph - Graph object representing all possible model pairings
+    :param: search_time_constraint - amount of time given to search
+    :param: model_time_constraint - amount of time output model pairing must 
                                     complete validation_dataset within
-    param: validation_dataset - dataset given to test each model pairing
-    param: conf_value_dataset - dataset given to find optimal confidence value
+    :param: validation_inputs - inputs given to test each model pairing using
+                               optimal confidence value
+    :param: validation_labels - corresponding labels
+    :param: conf_value_inputs - dataset given to find optimal confidence value
+    :param: conf_value_labels - corresponding labels
     output: tuple of models of size 2
 
     This method finds the model pairing chosen from the input model set
@@ -31,15 +35,16 @@ def greedy_walk(graph, search_time_constraint, model_time_constraint, \
 
         # Find current node's optimal confidence value.
         satisfy_model_time_constraint = \
-            current_node.find_and_set_optimal_confidence_value_grid(conf_value_dataset, \
-                                                                    model_time_constraint)
+            current_node.find_and_set_optimal_confidence_value_grid(\
+                conf_value_inputs, conf_value_labels, model_time_constraint)
 
         if satisfy_model_time_constraint:
             # Get the current node's test time and test accuracy using the optimal
             # confidence value on the validation dataset. If test time satisfies
             # the input model constraint and the test accuracy is better than
             # best_accuracy_so_far, update the variables.
-            test_accuracy, test_time = current_node.get_accuracy_and_time_optimal_conf(validation_dataset)
+            test_accuracy, test_time = current_node.get_accuracy_and_time_optimal_conf(\
+                    validation_inputs, validation_labels)
             
             if test_time < model_time_constraint:
                 best_node_so_far = current_node
