08e60649b790bbf416622065ebf195f41382f77e
Andrew Wei <andrewwei98@gmail.com>
2020-03-30 23:16:16 -0700

    implement accuracy function for Node class

--- GreedyWalk/graph.py
+++ GreedyWalk/graph.py
@@ -1,6 +1,7 @@
 import random
 import time
 import numpy as np
+import tensorflow as tf
 
 class Model():
     def __init__(self, model):
@@ -22,7 +23,7 @@ class Node():
 
         self.optimal_confidence_value = None
 
-    def find_and_set_optimal_confidence_value_grid(self, dataset, model_time_constraint, \
+    def find_and_set_optimal_confidence_value_grid(self, inputs, labels, model_time_constraint, \
             step_size = 0.1):
         '''
         For each possible confidence value between 0 and 1 with input step_size, run the 
@@ -38,7 +39,7 @@ class Node():
         satisfy_model_time_constraint = False
         for conf_value in confidence_values:
             before = time.time()
-            current_accuracy = self.accuracy(dataset, conf_value)
+            current_accuracy = self.accuracy(inputs, labels, conf_value)
             after = time.time()
 
             if after - before < model_time_constraint:
@@ -53,18 +54,55 @@ class Node():
         
         return satisfy_model_time_constraint
 
-    def find_and_set_optimal_confidence_value_random(self, dataset, model_time_constraint, step_size = 0.1):
+    def find_and_set_optimal_confidence_value_random(self, inputs, labels,\
+            model_time_constraint, step_size = 0.1):
         pass
 
-    def accuracy(self, dataset, conf_value):
+    def accuracy(self, inputs, labels, conf_value):
         '''
-        Get the accuracy of this combined model with the given confidence value.
+        :return: float - Get the accuracy of this combined model with the given confidence value.
         '''
-        pass
+        simple_model = self.model_1 if np.argmin(self.complexities) == 0 else self.model_2
+        complex_model = self.model_2 if np.argmin(self.complexities) == 0 else self.model_1
 
-    def get_accuracy_and_time_optimal_conf(self, dataset):
+        # -----------------------------------
+        # All Simple
+        simple_probs = simple_model.predict(inputs)
+        simple_highest_probs = np.amax(simple_probs, axis=1)
+
+        # -----------------------------------
+        # Complex predictions: get inputs of complex predictions
+        indices = [i for i in range(inputs.shape[0])]
+        complex_indices = np.where(simple_highest_probs < conf_value, indices, None)
+        complex_indices = complex_indices[complex_indices != np.array(None)] # remove None values
+        complex_indices = np.asarray(complex_indices, dtype=np.int64)
+
+        complex_inputs = np.take(inputs, complex_indices, axis=0)
+        if complex_inputs.shape[0] == 0:
+            complex_preds = []
+        else:
+            complex_preds = np.argmax(self.trained_complex_all_digit_model.predict(complex_inputs), axis=1)
+        # -----------------------------------
+        # Select simple
+        simple_indices = np.where(simple_highest_probs >= conf_value, indices, None)
+        simple_indices = simple_indices[simple_indices != np.array(None)] # remove None values
+        simple_indices = np.asarray(simple_indices, dtype=np.int64)
+
+        reduced_simple_probs = np.take(simple_probs, simple_indices, axis=0)
+        simple_preds = reduced_simple_probs.argmax(axis=1)
+
+        # ------------------------------------
+        # Reorganize preds
+        combined_preds = np.arange(inputs.shape[0])
+
+        np.put(combined_preds, simple_indices, simple_preds)
+        np.put(combined_preds, complex_indices, complex_preds)
+
+        return tf.reduce_mean(tf.cast(tf.equal(combined_preds, labels), tf.float32)).numpy()
+
+    def get_accuracy_and_time_optimal_conf(self, inputs, labels):
         before = time.time()
-        accuracy = self.accuracy(dataset, self.optimal_confidence_value)
+        accuracy = self.accuracy(inputs, labels, self.optimal_confidence_value)
         after = time.time()
 
         return (accuracy, after - before)
