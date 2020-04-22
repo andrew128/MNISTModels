a5e605431098e02f0bf32a699380e5d13367c5c7
Andrew Wei <andrewwei98@gmail.com>
2020-04-21 20:04:50 -0700

    debug imagenet labels process

--- GreedyWalk/imagenet_labels_process.py
+++ GreedyWalk/imagenet_labels_process.py
@@ -3,15 +3,24 @@ import numpy as np
 
 def main():
     # Create labels vector for validation data 1-10,000
-    fileNameToID = np.load('fileNameToID.npy', allow_pickle=True)
-    image_dir = './val_subset'
+    fileNameToID = np.load('fileNameToID.npy', allow_pickle=True).item()
+    image_dir = './imagenet_sample_data'
+
+    idToLabel = np.load('synset_map.npy', allow_pickle=True).item()
+
     fns = os.listdir(image_dir)
     fns.sort()
+
     output = np.zeros((10000, 1))
     for i, fn in enumerate(fns):
-        output[i] = fileNameToID[fn]
+        fn = os.path.splitext(fn)[0]
+        print(fn, fileNameToID[fn], idToLabel[fileNameToID[fn]])
+        output[i] = idToLabel[fileNameToID[fn]]
     
     nparray = np.asarray(output)
+
+    print(nparray)
+    # print(nparray.shape)
 
     np.save('val_labels_10000.npy', nparray)
 
