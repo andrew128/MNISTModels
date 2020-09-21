6ecdda5bf977ed51b41c0889d8556999458fe81f
Jake Polatty <jakepolatty@gmail.com>
2020-09-21 12:09:47 -0400

    Added CIFAR-10 training structure

---
+++ DiffNumModelCombinations/oldModels/cifar10_model_trainer.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import time
+import numpy as np
+
+import helpers.helper_funcs as helpers
+import helpers.cifar_models as models
+
+def train_and_save_models(x_train, y_train):
+    # Train each of the models 
+    input_shape = (32, 32, 1)
+
+    # No hidden layers (printistic regression)
+    l0_model = models.get_trained_l0_all_digit_model(x_train, y_train)
+    l0_model.save('models/cifar/l0_model')
+
+    # 1 hidden layer
+    l1_model = models.get_trained_l1_all_digit_model(x_train, y_train)
+    l1_model.save('models/cifar/l1_model')
+
+    # 1 conv layer, 1 hidden layer
+    l2_model = models.get_trained_l2_all_digit_model(x_train, y_train, input_shape)
+    l2_model.save('models/cifar/l2_model')
+
+    # 2 conv layers, 1 hidden layer
+    l3_model = models.get_trained_l3_all_digit_model(x_train, y_train, input_shape)
+    l3_model.save('models/cifar/l3_model')
+
+    # 3 conv layers, 1 hidden layer
+    # l4_model = models.get_trained_l4_all_digit_model(x_train, y_train, input_shape)
+    # l4_model.save('models/l4_model')
+
+def main():
+    print('Loading data...')
+    x_train, y_train, x_test, y_test = helpers.get_cifar10_data()
+    num_classes = 10
+    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+
+    train_and_save_models(x_train, y_train)
+
+if __name__ == '__main__':
+    main()
